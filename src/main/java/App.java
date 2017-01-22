88994f6615ab3012b85bdbbeec397a0493b7da40
Jakub Kręt <Jakub Krt>
2017-01-22 12:49:07 +0100

    Add main class App.java and websocket handler

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static spark.Spark.staticFileLocation;
+import static spark.Spark.webSocket;
+
+/**
+ * Created by jakub.a.kret@gmail.com on 2017-01-22.
+ */
+public class App {
+    static List<Map<Session, String>> channels = new LinkedList<Map<Session, String>>();
+    static List<String> channelNames = new LinkedList<>();
+    static Map<Session, String> lobby = new ConcurrentHashMap<Session, String>();
+
+    public static void main(String[] args) {
+        Map<Session, String> general = new ConcurrentHashMap<Session, String>();
+        channels.add(general);
+        channelNames.add("General");
+        staticFileLocation("/public");
+        webSocket("/main", MainWebSocketHandler.class);
+    }
+}
---
+++ src/main/java/MainWebSocketHandler.java
@@ -0,0 +1,9 @@
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+
+/**
+ * Created by jakub.a.kret@gmail.com on 2017-01-22.
+ */
+@WebSocket
+public class MainWebSocketHandler {
+}
